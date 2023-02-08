8aa018b1e02eba7753fc5ca943da5ebd6a2c036d
milton giraldo <mil.f@hotmail.es>
2023-02-08 11:58:03 -0500

    prueba scraping

---
+++ ensayo5.py
@@ -0,0 +1,45 @@
+import pandas as pd 
+import openpyxl
+
+
+df=pd.DataFrame
+
+
+df1=pd.read_excel("raw_scrap28.xlsx", engine="openpyxl")
+df2=pd.read_excel("raw_scrap20.xlsx",engine="openpyxl")
+
+
+df3= pd.merge(left=df1["URL"],right=df2["URL"],how="left")
+df3. to_excel("resultado2.xlsx")
+
+
+
+
+
+#df3=pd.read_excel("resultado2.xlsx",engine="openpyxl")
+
+#df4= pd.merge(df3["URL"],df1["URL"],how="inner")
+#df4. to_excel("resultado4.xlsx")
+
+#df4= pd.merge(df3["URL"],df2["URL"],how="inner")
+#df4. to_excel("resultado3.xlsx")
+
+#df3= pd.merge(left=df1["URL"],right=df2["URL"],how="left")
+#df3. to_excel("resultado2.xlsx")
+
+#df3= pd.merge(df1["URL"],df2["URL"],how="inner").head(100)
+#df3. to_excel("resultado.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
