6f73db178a95af9e1f19f486cfeb5e2dc9defa52
everduvan <everduvanhdez@gmail.com>
2023-02-10 14:52:21 -0500

    update

--- ensayo5.py
+++ ensayo5.py
@@ -1,16 +1,20 @@
 import pandas as pd 
 import openpyxl
 
-
 df=pd.DataFrame
 
-
 df1=pd.read_excel("raw_scrap28.xlsx", engine="openpyxl")
+print(df1)
 df2=pd.read_excel("raw_scrap20.xlsx",engine="openpyxl")
+print(df2)
 
+#df3= pd.merge(left=df1["URL"],right=df2["URL"],how="left") # blue
+#df3= pd.merge(left=df1["URL"],right=df2["URL"],how="right") # pink
+#df3= pd.merge(left=df1["URL"],right=df2["URL"],how="inner") # red-repetidos
+df3= pd.merge(left=df1["URL"],right=df2["URL"],how="outer")# white-no repetidos
+print(df3)
 
-df3= pd.merge(left=df1["URL"],right=df2["URL"],how="left")
-df3. to_excel("resultado2.xlsx")
+#df3. to_excel("test.xlsx")
 
 
 
@@ -29,17 +33,4 @@ df3. to_excel("resultado2.xlsx")
 
 #df3= pd.merge(df1["URL"],df2["URL"],how="inner").head(100)
 #df3. to_excel("resultado.xlsx")
-
-
-
-
-
-
-
-
-
-
-
-
-
 
